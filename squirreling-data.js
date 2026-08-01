6b96e5511ce6a5f9f26ae95e418eaccaae3be0bd
Sophie Blackwood <122307675+sophieBlackwood@users.noreply.github.com>
2026-08-01 14:12:48 -0600

    Update squirreling-data.js

--- squirreling-data.js
+++ squirreling-data.js
@@ -1,12 +1,46 @@
 const emailsData = [
-    { title: "First Email", date: "2026-01-01", preview: "Short preview snippet here...", content: "Full content..." },
-    { title: "Second Email", date: "2026-01-08", preview: "Short preview snippet here...", content: "Full content..." },
-    { title: "Third Email", date: "2026-01-15", preview: "Short preview snippet here...", content: "Full content..." },
-    { title: "Fourth Email", date: "2026-01-22", preview: "Short preview snippet here...", content: "Full content..." },
-    { title: "Fifth Email", date: "2026-01-29", preview: "Short preview snippet here...", content: "Full content..." },
-    { title: "Sixth Email", date: "2026-02-05", preview: "Short preview snippet here...", content: "Full content..." },
-    { title: "Seventh Email", date: "2026-02-12", preview: "Short preview snippet here...", content: "Full content..." },
-    { title: "Eighth Email", date: "2026-02-19", preview: "Short preview snippet here...", content: "Full content..." },
-    { title: "Ninth Email", date: "2026-02-26", preview: "Short preview snippet here...", content: "Full content..." },
-    { title: "Tenth Email", date: "2026-03-05", preview: "Short preview snippet here...", content: "Full content..." }
+    { title: "Hi, I'm Ricky", date: "2026-01-01", content: "
+        Brooklyn: Hey Dallin,
+I don't know the correct procedure to email missionaries and who knows if/when you'll actually get this but I wanted to reach out. I waited until I was in a better mood so hopefully this email will bring you some joy.
+
+It's definitely been a little hard adjusting lol I read your last text in the middle of seminary graduation and told myself I wasn't going to cry. I very badly failed but yeah lol it was fine. The end of school was fun, I was able to watch graduation via livestream and then I had to pick up my brother who was playing in the band so I drove down and parked a quarter mile away and walked because Parke is a mess and I didn't want to deal with grad traffic. I was able to see some of the seniors leaving and it was fun to have that last goodbye with them.
+
+Earlier that day (Wednesday) I also got together with Gram and Sophie to make our unit circle cake. It wasn't super pretty but it did taste good thankfully. We got worried because we made a second cake and did the blind deaf and mute challenge and that one tasted.....interestingly lol. Gram was blind (originally I typed blind was Gram and stared at it wondering why it didn't make sense) and I was deaf (VERY much in my own little world listening to Ricky Jamarez and other stuff, it was hard for them to get my attention because I was too busy dancing) and Sophie was mute. It was a lot of fun lol and ended up taking a long time so it kept my mind off things and really helped a bunch.
+
+I will admit this weekend has been really hard and lonely but I'm working through it. It really has been weird not being able to talk to you through everything but I've been talking to you in my mind (yeah I'm insane lol I've lost it) and a lot of the things you've said that have stuck with me have helped a lot. I got invited to help with an Eagle Scout service project so I spent a few hours Saturday sanding and staining wood. I was the only girl there which was odd but I knew all the guys and it was fun and let me kinda clear my mind which was good because Saturday was the hardest for me. I've also been working a lot on my DYW video and trying to get as much as I can done there.
+
+My summer classes both start tomorrow and I'm a bit worried for the significantly increased workload because these are only college classes not dual credit and move a lot faster. I'm also working 20 hours next week which is going to be killer lol especially since I'm working alone but maybe I'll download a book on Libby or listen to General Conference or somethin. Who knows. My work is switching over to a clock in clock out system so I had to create a new email so this might be the current email to use but you can email any of the 3 emails I've used for you lol oops sorry. 
+
+I've been talking about you all week, I'm pretty sure everyone I know at this point knows I'm so excited for you. I'm curious how things are going and as hard as it is on my side I really am so glad to have you focusing on the gospel and missionary work. 
+
+Anyways here is my ramble I refuse to read back because I've always just said what's on my mind to you and not read it back so enjoy maybe? I don't know. Lol I haven't been texting or talking to anyone now that I'm just kinda alone or out of it in most places so the awkwardness has not improved. I attached a few pictures, I don't know if you can look at them but enjoy if you can?
+
+Be safe and know there's so many people cheering you on. 🫶
+    
+    Dallin: Hey Brooklyn that all means a lot to me IDK if you would like me to reply to this email address or the other, but I miss you a lot as well and that means a lot to me I wish I had more time to respond to you and everyone but hopefully the hour and a half email is good and you enjoy the photos in the other email I sent. It touched my heart to read that you talked and shared about me and I hope you know and that it means something that I put your name on the time when I went. I'd love to hear more of how u r but time flies and I'm not sure how much I can respond as I said.
+
+Stay safe 
+-Elder Stahmann 
+        
+Brooklyn: Lol it feels really wrong to be tearing up while monitoring children watching Jurrasic Park (I'm at work) but I'm also listening to Like a Child which is a song that has deep meaning to me, listen to it if you get the chance, it's on the 2018 youth album. I don't mind either email, I got the forwarding set up but this is my main email now. I don't mind you not responding, it's just nice to know you're there. I'll probably just keep the weekly updates going and love reading your stuffs. I'm glad you're doing well and you made it. Take care and be safe. 🫶♾️
+Brooklyn
+    
+Dallin: Hi again Brooklyn I had a bit of extra time so I wanted to say hi and just say thanks again and I'm sending you my best wishes. My weather app says Burley is on fire... you are still alive correct? Also don't forget to eat and stay safe
+
+-Elder Dallin Stahmann
+    
+Brooklyn: Howdy, I'm glad you're taking extra time for me. Yes Burley is having a lot of issues with fire right now but currently I am alive although my boss is trying to get me to come in and pick up more hours at work. 21 is enough hours to work in a week for me though and I need to pack for FSY. Remind me to send you pictures of the bazillion octopi I crocheted to give to my company and my BOM I designed for FSY as well. I've been forgetting to eat lol yesterday I got pretty sick but I'm working on it. I was just listening to Microwave so this was nice timing. Best wishes as well and may you stay safe in a way that makes for good stories. 
+The Goddess of Dandelions
+    
+
+You probably won't see this but as part of FSY next week I will get to go to the MTC to watch the Tuesday Devo. So that will be fun and maybe I'll see you (but honestly probably not lol but the idea is fun)" },
+    { title: "Second Email", date: "2026-01-08", content: "Full content..." },
+    { title: "Third Email", date: "2026-01-15", content: "Full content..." },
+    { title: "Fourth Email", date: "2026-01-22", content: "Full content..." },
+    { title: "Fifth Email", date: "2026-01-29", content: "Full content..." },
+    { title: "Sixth Email", date: "2026-02-05", content: "Full content..." },
+    { title: "Seventh Email", date: "2026-02-12", content: "Full content..." },
+    { title: "Eighth Email", date: "2026-02-19", content: "Full content..." },
+    { title: "Ninth Email", date: "2026-02-26", content: "Full content..." },
+    { title: "Tenth Email", date: "2026-03-05", content: "Full content..." }
 ];
